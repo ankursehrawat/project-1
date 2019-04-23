0b1db674cb7134443cbef713a408cb6faed0e674
500069625 <46739417+500069625@users.noreply.github.com>
2019-04-23 23:56:27 +0530

    addition of few lines

--- railwaymanagement.cpp
+++ railwaymanagement.cpp
@@ -1,33 +1,61 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<iostream.h>
-#include<time.h>
-#include<iomanip.h>
-#include<fstream.h>
-char f[10]="f";
-char s[10]="s";
-int addr,ad,flag,f1,d,m,i,amt;
-float tamt
-class login
-{
-public:
-    char id[100];
-    char pass[100];
-    char *password;
-    void getid()
+ float amc;
+    void getresdet()
     {
-        cout<<"Enter your id:";
-        gets(id);
-        password=getpass("Enter the password:");
-        strcpy(pass,password);
+        cout<<"Enter the details as follows\n";
+        cout<<"Train no:";
+        cin>>tno;
+        cout<<"Train name:";
+        gets(tname);
+        cout<<"Boarding point:";
+        gets(bp);
+        cout<<"Destination pt:";
+        gets(dest);
+        cout<<"No of seats required:";
+        cin>>nosr;
+        for(i=0; i<nosr ; i++)
+        {
+            cout<<"Passenger name:";
+            gets(pname[i]);
+            cout<<"Passenger age:";
+            cin>>age[i];
+        }
+        cout<<"Enter the class f-first class s-second class:";
+        gets(clas);
+        cout<<"Date of travel:";
+        cin>>d>>m>>y;
+        cout<<"Enter the concession category\n";
+        cout<<"1.Military\n2.Senior citizen\n";
+        cout<<"3.Children below 5 yrs\n4.None\n";
+        cin>>con;
+        cout<<"............END OF GETTING DETAILS............\n";
     }
-    void displayid()
+    void displayresdet()
     {
-        cout<<"Id:";
-        puts(id);
-        cout<<"Password:";
-        puts(pass);
+        cout<<"...............................................\n";
+        cout<<"...............................................\n";
+        cout<<"Pnr no:"<<pnr;
+        cout<<"\nTrain no:"<<tno;
+        cout<<"\nTrain name:";
+        puts(tname);
+        cout<<"Boarding point:";
+        puts(bp);
+        cout<<"Destination pt:";
+        puts(dest);
+        cout<<"No of seats reserved:"<<nosr;
+        for(i=0; i<nosr; i++)
+        {
+            cout<<"Passenger name:";
+            puts(pname[i]);
+            cout<<"Passenger age:"<<age[i];
+        }
+
+        cout<<"\nYour class:";
+        puts(clas);
+        cout<<"\nDate of reservation:"<<d<<"-"<<m<<"-"<<y;
+        cout<<"\nYour concession category:"<<con;
+        cout<<"\nYou must pay:"<<amc<<endl;
+        cout<<"***********************************************\n";
+        cout<<".........END OF RESERVATION.................\n";
+        cout<<"***********************************************\n";
     }
 };
