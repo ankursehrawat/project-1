7bd14a2208f6c5af4950502694541efed68d1231
ankursehrawat <46739506+ankursehrawat@users.noreply.github.com>
2019-04-24 00:35:42 +0530

    Update railwaymanagement.cpp

--- railwaymanagement.cpp
+++ railwaymanagement.cpp
@@ -110,3 +110,270 @@ public:
     int i;
     int d,m,y;
     int con
+oid manage();
+void can();
+void user();
+void database();
+void res();
+void reserve();
+void displaypassdetail();
+void cancell();
+void enquiry();
+void main()
+{
+    clrscr();
+    int ch;
+    cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    cout<<".......WELCOME TO RAILWAY RESERVATION SYSTEM..........\n";
+    cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    do
+    {
+        cout<<"^^^^^^^^^^^^^^^^^^^^^^MAIN MENU^^^^^^^^^^^^^^^^^^^^\n";
+        cout<<"1.Admin mode\n2.User mode\n3.Exit\n";
+        cout<<"Enter your choice:";
+        cin>>ch;
+        cout<<endl;
+        switch(ch)
+
+        {
+        case 1:
+            database();
+            break;
+        case 2:
+            user();
+            break;
+        case 3:
+            exit(0);
+        }
+    }
+    while(ch<=3);
+    getch();
+}
+void database()
+{
+    char *password;
+    char *pass="codewithc";
+    password=getpass("Enter the admininistrator password:");
+    detail a;
+    fstream f;
+    int ch;
+    char c;
+    if(strcmp(pass,password)!=0)
+    {
+        cout<<"Enter the password correctly \n";
+        cout<<"You are not permitted to logon this mode\n";
+        goto h;
+    }
+    if(strcmp(pass,password)==0)
+    {
+        char c;
+        do
+        {
+            cout<<"...........ADMINISTRATOR MENU...........\n";
+            cout<<"1.Create detail data base\n2.Add details\n";
+            cout<<"3.Display details\n4.User management\n";
+            cout<<"5.Display passenger details\n6.Return to main menu\n";
+            cout<<"Enter your choice:";
+            cin>>ch;
+
+
+            cout<<endl;
+            switch(ch)
+            {
+            case 1:
+                f.open("t.txt",ios::out|ios::binary);
+                do
+                {
+                    a.getdetail();
+                    f.write((char *) & a,sizeof(a));
+                    cout<<"Do you want to add one more record?\n";
+                    cout<<"y-for Yes\nn-for No\n";
+                    cin>>c;
+                }
+                while(c=='y');
+                f.close();
+                break;
+            case 2:
+                f.open("t.txt",ios::in|ios::out|ios::binary|ios::app);
+                a.getdetail();
+                f.write((char *) & a,sizeof(a));
+                f.close();
+                break;
+            case 3:
+                f.open("t.txt",ios::in|ios::out|ios::binary|ios::app);
+                f.seekg(0);
+                while(f.read((char *) & a,sizeof(a)))
+                {
+                    a.displaydetail();
+                }
+                f.close();
+                break;
+            case 4:
+                manage();
+                break;
+            case 5:
+                displaypassdetail();
+                break;
+            }
+        }
+        while(ch<=5);
+
+        f.close();
+    }
+h:
+}
+void reserve()
+{
+    int ch;
+    do
+    {
+        cout<<"1.Reserve\n2.Return to the main menu\n";
+        cout<<"Enter your choice:";
+        cin>>ch;
+        cout<<endl;
+        switch(ch)
+        {
+        case 1:
+            res();
+            break;
+        }
+    }
+    while(ch==1);
+    getch();
+}
+void res()
+{
+    detail a;
+    reser b;
+    fstream f1,f2;
+    time_t t;
+    f1.open("t.txt",ios::in|ios::out|ios::binary);
+    f2.open("p.txt",ios::in|ios::out|ios::binary|ios::app);
+    int ch;
+    b.getresdet();
+    while(f1.read((char *) &a,sizeof(a)))
+    {
+        if(a.tno==b.tno)
+        {
+            if(strcmp(b.clas,f)==0)
+            {
+                if(a.c1>=b.nosr)
+
+                {
+                    amt=a.c1fare;
+                    addr=f1.tellg();
+                    ad=sizeof(a.c1);
+                    f1.seekp(addr-(7*ad));
+                    a.c1=a.c1-b.nosr;
+                    f1.write((char *) & a.c1,sizeof(a.c1));
+                    if(b.con==1)
+                    {
+                        cout<<"Concession category:MILITARY PERSONNEL\n";
+
+                        b.amc=b.nosr*((amt*50)/100);
+                    }
+                    else if(b.con==2)
+                    {
+                        cout<<"Concession category:SENIOR CITIZEN\n";
+                        b.amc=b.nosr*((amt*60)/100);
+                    }
+                    else if(b.con==3)
+                    {
+                        cout<<"Concession category:CHILDERN BELOW FIVE\n";
+                        b.amc=0.0;
+                    }
+                    else if(b.con==4)
+                    {
+                        cout<<"You cannot get any concession\n";
+                        b.amc=b.nosr*amt;
+                    }
+                    srand((unsigned) time(&t));
+                    b.pnr=rand();
+                    f2.write((char *) & b,sizeof(b));
+                    b.displayresdet();
+                    cout<<"------------------------------------------------------\n";
+                    cout<<"--------------Your ticket is reserved-----------\n";
+                    cout<<"-----------------End of reservation menu-------\n";
+                }
+                else
+                {
+                    cout<<"**********Sorry req seats not available********\n";
+
+                }
+            }
+            else if(strcmp(b.clas,s)==0)
+            {
+                if(a.c2>=b.nosr)
+                {
+                    amt=a.c2fare;
+                    addr=f1.tellg();
+                    ad=sizeof(a.c2);
+                    f1.seekp(addr-(5*ad));
+                    a.c2=a.c2-b.nosr;
+                    f1.write((char *) & a.c2,sizeof(a.c2));
+                    if(b.con==1)
+                    {
+                        cout<<"Concession category:MILITARY PRESONNEL\n";
+                        b.amc=b.nosr*((amt*50)/100);
+                    }
+                    else if(b.con==2)
+                    {
+                        cout<<"Concession category:SENIOR CITIZEN\n";
+                        b.amc=b.nosr*((amt*60)/100);
+                    }
+                    else if(b.con==3)
+                    {
+                        cout<<"Concession category:CHILDERN BELOW FIVE\n";
+                        b.amc=0.0;
+                    }
+                    else if(b.con==4)
+                    {
+                        cout<<"You cannot get any concession\n";
+                        b.amc=b.nosr*amt;
+                    }
+                    f2.write((char *) & b,sizeof(b));
+                    b.displayresdet();
+                    cout<<"---------------------------------------\n";
+                    cout<<"--------Your ticket is reserved--------\n";
+                    cout<<"------------End of reservation---------\n";
+                }
+                else
+
+
+                {
+                    cout<<"********Sorry req no of seats not available*******\n";
+                }
+            }
+            getch();
+
+            goto h;
+        }
+        else
+        {
+            flag=0;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"............Wrong train no......................\n";
+        cout<<"......Enter the train no from the data base.....\n";
+    }
+    f1.close();
+    f2.close();
+    getch();
+h:
+}
+void displaypassdetail()
+{
+    fstream f;
+    reser b;
+    f.open("p.txt",ios::in|ios::out|ios::binary);
+    f.seekg(0);
+    while(f.read((char *) & b,sizeof(b)))
+    {
+        b.displayresdet();
+    }
+    f.close();
+    getch();
+}
+void enquiry()
